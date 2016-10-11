720c3dc52008c7d68dcafee7ea26760c9af552fc
Cheng Gu <guchengf@gmail.com>
2016-10-11 11:58:12 +0800

    Create http-event.service.ts

---
+++ http-event.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+type HttpEventType = "START" | "FINISH";
+
+export function httpEmitter(target, key, descriptor) {
+    return Object.assign({}, descriptor, {
+        value: function () {
+            httpEventServiceFactory.emitHttpEvent('START');
+            const subscription = descriptor.value.apply(this, arguments);
+            subscription.subscribe(() => {
+                httpEventServiceFactory.emitHttpEvent('FINISH');
+            });
+            return subscription;
+        }
+    });
+}
+
+@Injectable()
+export class HttpEventService {
+    private httpEvent = new BehaviorSubject<HttpEventType>(null);
+
+    httpEventCenter = this.httpEvent.asObservable();
+
+    constructor() {
+    }
+
+    emitHttpEvent(type: HttpEventType) {
+        return this.httpEvent.next(type);
+    }
+}
+
+export const httpEventServiceFactory = new HttpEventService();
